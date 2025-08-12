550e4f0cebba4c5b37e97fbef84858ea2c7bc9ec
Jean Paul Astonitas <2745716+jastonitas@users.noreply.github.com>
2025-08-12 00:42:20 -0500

    Update installs.sh

--- clase03/installs.sh
+++ clase03/installs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Instalar Postgres local y soporte para python
-!apt-get -qq update -y
-!apt-get -qq install -y postgresql postgresql-contrib
-!pip install -q psycopg2-binary
-!pip install -q sqlalchemy psycopg2-binary
+apt-get -qq update -y
+apt-get -qq install -y postgresql postgresql-contrib
+pip install -q psycopg2-binary
+pip install -q sqlalchemy psycopg2-binary
