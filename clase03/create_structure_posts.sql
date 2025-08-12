db847aa33640a76260b6223b404b25c6dd0d6162
Jean Paul Astonitas <2745716+jastonitas@users.noreply.github.com>
2025-08-12 00:26:25 -0500

    Create create_structure_posts.sql

---
+++ clase03/create_structure_posts.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS follows;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE "follows" (
+  "following_user_id" integer,
+  "followed_user_id" integer,
+  "created_at" timestamp
+);
+
+CREATE TABLE "users" (
+  "id" integer PRIMARY KEY,
+  "username" varchar,
+  "role" varchar,
+  "created_at" timestamp
+);
+
+CREATE TABLE "posts" (
+  "id" integer PRIMARY KEY,
+  "title" varchar,
+  "body" text,
+  "user_id" integer NOT NULL,
+  "status" varchar,
+  "created_at" timestamp
+);
+
+COMMENT ON COLUMN "posts"."body" IS 'Content of the post';
+
+ALTER TABLE "posts" ADD CONSTRAINT "user_posts" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "follows" ADD FOREIGN KEY ("following_user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "follows" ADD FOREIGN KEY ("followed_user_id") REFERENCES "users" ("id");
